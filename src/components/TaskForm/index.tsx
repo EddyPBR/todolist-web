6073106cb68475e61a1073d68ac94adff68c60c7
EddyPBR <edvaldojunior1310@gmail.com>
2023-10-14 18:41:57 -0300

    :sparkles: implements TaskForm component

---
+++ src/components/TaskForm/index.tsx
@@ -0,0 +1,66 @@
+import { Form, Input, Select, Button, Space } from "antd";
+
+export type ITaskPriorityEnum = "HIGH" | "MEDIUM" | "LOW";
+
+export interface ITaskForm {
+  title: string;
+  description: string;
+  priority: ITaskPriorityEnum;
+}
+
+export interface ITaskFormProps {
+  id?: string;
+  onCancel?: () => void;
+}
+
+const SELECT_OPTIONS = [
+  { value: "HIGH", label: "Alta" } as const,
+  { value: "MEDIUM", label: "Media" } as const,
+  { value: "LOW", label: "Baixa" } as const,
+];
+
+export function TaskForm({ id, onCancel }: ITaskFormProps) {
+  function handleSubmitForm(formData: ITaskForm) {
+    if (id) {
+      alert("Chamou a função de atualizar tarefa: " + "\n" + JSON.stringify(formData));
+    } else {
+      alert("Chamou a função de criar tarefa: " + "\n" + JSON.stringify(formData));
+    }
+  }
+
+  return (
+    <Form name="task" onFinish={handleSubmitForm} layout="vertical" style={{ width: "100%", maxWidth: 480 }}>
+      <Form.Item<ITaskForm> name="title" label="Título" rules={[{ required: true, message: "Título é obrigatório" }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item<ITaskForm>
+        name="description"
+        label="Descrição"
+        rules={[{ required: true, message: "Descrição é obrigatório" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item<ITaskForm>
+        name="priority"
+        label="Descrição"
+        rules={[{ required: true, message: "Descrição é obrigatório" }]}
+        initialValue="MEDIUM"
+      >
+        <Select<ITaskPriorityEnum> defaultValue="MEDIUM" options={SELECT_OPTIONS} />
+      </Form.Item>
+
+      <Form.Item>
+        <Space>
+          <Button type="primary" htmlType="submit">
+            {id ? "Atualizar" : "Criar"}
+          </Button>
+          <Button onClick={onCancel} htmlType="reset">
+            {onCancel ? "Cancelar" : "Limpar"}
+          </Button>
+        </Space>
+      </Form.Item>
+    </Form>
+  );
+}
