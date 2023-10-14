253bd180a7e6e116ca7bcb861eb5fe41b5de5efe
EddyPBR <edvaldojunior1310@gmail.com>
2023-10-14 20:13:38 -0300

    :sparkles: enable AppLayout in all pages of application

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { TasksPage } from "./pages/TasksPage";
+import { AppLayout } from "./Layout/AppLayout";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cadastro" element={<RegisterPage />} />
-        <Route path="/tarefas" element={<TasksPage />} />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cadastro" element={<RegisterPage />} />
+          <Route path="/tarefas" element={<TasksPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
